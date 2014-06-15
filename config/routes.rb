f6f35575ba36af176dc70be165b5dfdf0ffeec27
bemyhre <ben.myhre35@gmail.com>
2014-06-15 13:15:56 -0700

    backtrack a little

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Handytapp::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -46,9 +47,8 @@ Handytapp::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
+
 
   # See how all your routes lay out with "rake routes"
 
