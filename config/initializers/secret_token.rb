b4584b6eef55e35f3a8df7bc3e37351155fa7b3f
bemyhre <ben.myhre35@gmail.com>
2014-06-15 10:48:02 -0700

    more first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Handytapp::Application.config.secret_token = '498dbc3a8861e841647d6d7e9c5b0b262b4f8718d5fbe8ec20a94369ccfbd6abf45127f756e645531e8d06d64a3ac20dbbf46f667fd098d470d195202d7933f7'
