42cdc61b83f859529671819326f205f5f63ebc74
bemyhre <ben.myhre35@gmail.com>
2014-06-15 16:41:55 -0700

    added if else on login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.authenticate(params[:email], params[:password])
+  	if user
+  		session[:id] = user.id
+  		redirect_to root_url, :notice => "Logged in!"
+  	else
+  		flash.now.alert = "Invalid credentials"
+  		render "new"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Handytapp::Application.routes.draw do
   get "sessions/new"
 
   get "sign_up" => "users#new", :as => "sign_up"
+  get "log_in" => "sessions#new", :as =>"log_in"
+  
   root :to => "users#new"
+  
   resources :users
+  resources :sessions
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
