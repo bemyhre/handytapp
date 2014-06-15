5e22b285dfd4dec5f1e492e79722c1bb04780ebf
bemyhre <ben.myhre35@gmail.com>
2014-06-15 15:12:41 -0700

    add users and sessions (up to the point of logging in)

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :fname, :lname, :password, :password_confirmation
+  attr_accessor :password
+
+  before_save :encrypt_password
+
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create 
+  validates_presence_of :email, :on => :create 
+  validates_presence_of :fname, :on => :create 
+  validates_presence_of :lname, :on => :create 
+  validates_uniqueness_of :email
+
+  def encrypt_password
+  	if password.present?
+  		self.password_salt= BCrypt::Engine.generate_salt
+  		self.password_hash= BCrypt::Engine.hash_secret(password, password_salt)
+  	end
+  end
+
+end
