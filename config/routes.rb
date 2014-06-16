c8014dcf3c700d82e3ea673e2f25e6c3a5682a68
bemyhre <ben.myhre35@gmail.com>
2014-06-15 17:34:14 -0700

    added logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Handytapp::Application.routes.draw do
 
   get "sign_up" => "users#new", :as => "sign_up"
   get "log_in" => "sessions#new", :as =>"log_in"
+  get "log_out" => "sessions#destroy", :as =>"log_out"
   
   root :to => "users#new"
   
