c4f84a8a2c5013b7b27bd54152ab1ed2fbe5afe5
bemyhre <ben.myhre35@gmail.com>
2014-06-16 21:00:36 -0700

    added partial beers form

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,5 +1,5 @@
 class Beer < ActiveRecord::Base
-  attr_accessible :brewery, :name, :rating, :style
+  attr_accessible :brewery_id, :name, :rating, :style
   has_one :brewery
   has_many :bars, through: :barmenus
 end
