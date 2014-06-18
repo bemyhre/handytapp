3835d6b5e71ca72c358372f786a776842f508410
bemyhre <ben.myhre35@gmail.com>
2014-06-17 21:03:54 -0700

    tried to fix form... going to bed

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,5 +1,5 @@
 class Beer < ActiveRecord::Base
-  attr_accessible :brewery_id, :name, :rating, :style
+  attr_accessible :brewery_id, :name, :rating, :style_id
   has_one :brewery
   has_one :style
   has_many :bars, through: :barmenus
--- db/migrate/20140616020754_create_beers.rb
+++ db/migrate/20140616020754_create_beers.rb
@@ -1,10 +1,9 @@
 class CreateBeers < ActiveRecord::Migration
   def change
     create_table :beers do |t|
-      t.belongs_to :brewery
       t.belongs_to :style
+      t.belongs_to :brewery
       t.string :name
-      t.string :style
       t.float :rating
       t.string :image
       t.timestamps
