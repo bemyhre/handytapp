c5c40feace0d6c308372fb1d82a4be43e3481e57
bemyhre <ben.myhre35@gmail.com>
2014-06-21 12:50:43 -0700

    added model information

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -2,7 +2,7 @@ class Bar < ActiveRecord::Base
   attr_accessible :address, :city, :state, :coordinates, :grade, :name, :zip
   has_many :beers, through: :barmenus
   geocoded_by :all_addresses
-  after_validation :geocode
+  after_validation :geocode, :if => :all_addresses_changed?
 
   def all_addresses
   	"#{address} #{city} #{state} #{zip}"
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,7 +2,7 @@ class Brewery < ActiveRecord::Base
   attr_accessible :address, :city, :coordinates, :name, :rating, :zip, :state
   has_many :beers
   geocoded_by :all_addresses
-  after_validation :geocode
+  after_validation :geocode, :if => :all_addresses_changed?
 
   def all_addresses
   	"#{address} #{city} #{state} #{zip}"
