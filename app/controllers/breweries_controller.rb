0369e2039dd1c377ea95a45544496639c4241eec
bemyhre <ben.myhre35@gmail.com>
2014-06-21 12:06:15 -0700

    added geocoding for breweries

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,4 +1,14 @@
 class BreweriesController < ApplicationController
   def new
+  	@brewery=Brewery.new
+  end
+  
+  def create
+  	@brewery=Brewery.new(params[:brewery])
+  	if @brewery.save
+  		redirect_to root_url, :notice =>"Brewery added!"
+  	else
+  		render "new"
+  	end
   end
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,10 @@
 class Brewery < ActiveRecord::Base
-  attr_accessible :address, :city, :coordinates, :name, :rating, :zip
+  attr_accessible :address, :city, :coordinates, :name, :rating, :zip, :state
   has_many :beers
+  geocoded_by :all_addresses
+  after_validation :geocode
+
+  def all_addresses
+  	"#{address} #{city} #{state} #{zip}"
+  end 
 end
