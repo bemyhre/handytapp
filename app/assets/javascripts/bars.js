671831f7dc6c20a2ed365b5687cbeefbe3068be2
bemyhre <ben.myhre35@gmail.com>
2014-06-22 21:50:27 -0700

    added some map options, styling, logo and a few changes. TODO Ajax to tie map to db entries

--- app/assets/javascripts/bars.js
+++ app/assets/javascripts/bars.js
@@ -37,25 +37,28 @@ function renderCurrentLocation(lat,long){
 
   map = new google.maps.Map(document.getElementById('map'), {
       center: place,
-      zoom: 15
+      zoom: 15,
+      disableDefaultUI: true,
+      zoomControl: true,
+      zoomControlOptions: {
+      	position: google.maps.ControlPosition.TOP_RIGHT
+      }
     });
 
   var request = {
     location: place,
-    radius: '5000',
-    types: ['bar']
+    radius: '7000',
+    types: ['bar'],
   };
 
   service = new google.maps.places.PlacesService(map);
    service.nearbySearch(request, callback);
-
 }
 
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       place = results[i];
-      //console.log(place)
       createMarker(results[i]);
     }
   }
@@ -65,32 +68,23 @@ function createMarker(result) {
 	var lat = result.geometry.location.k
 	var long = result.geometry.location.A
 	var position=new google.maps.LatLng(lat,long)
-	//var contentString = html;
 	var marker = new google.maps.Marker({
 	    position: position,
 	    map: map,
 	    zIndex: Math.round(position.lat()*-100000)<<5
 	    });
-	//console.log(marker)
-	console.log("#######################")
 	name=result.name
 	addy=result.vicinity
-	console.log(addy)
-	//console.log(map)
-
   var infowindow = new google.maps.InfoWindow({
       content: name + " " + addy
   });
 
 	google.maps.event.addListener(marker, 'mouseover', function() {
-	    //infowindow.setContent(content); 
 	    infowindow.open(map,marker);
 	});
 
 	google.maps.event.addListener(marker, 'mouseout', function() {
-	    //infowindow.setContent(content); 
 	    infowindow.close();
 	});
-	//gmarkers.push(marker);
 	marker.setMap(map);
 }
